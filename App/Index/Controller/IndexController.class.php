2b904f9b03fe7d6e2ca2da5819ada5dd6566c095
nagatoyk <393117228@qq.com>
2014-10-08 01:52:24 +0800

    123

--- App/Index/Controller/IndexController.class.php
+++ App/Index/Controller/IndexController.class.php
@@ -1,44 +1,135 @@
 <?php
 /**
- * 前台
+ * 萌否
  */
 class IndexController extends Controller{
-    /**
-     * 初始化
-     */
-    public function __construct(){
-        parent::__construct();
-    }
-    /**
-     * 首页
-     */
-    public function index(){
-        $this->display('index.v2.html');
-    }
-    /**
-     * 抓取播放列表
-     */
-    public function catch_listen(){
-        header('Content-Type:application/json;charset=utf-8');
-        $url = 'http://moe.fm/listen/playlist?';
-        echo $this->curl($url.$_SERVER['QUERY_STRING']);
-
-    }
-    /**
-     * 抓取个人信息
-     */
-    public function catch_detail(){
-        header('Content-Type:application/json;charset=utf-8');
-        $url = 'http://api.moefou.org/user/detail.json?';
-        echo $this->curl($url.$_SERVER['QUERY_STRING']);
-    }
-    private function curl($url){
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return $data;
-    }
+	private $_qqweibo = '';
+	private $_moefm = '';
+	public function __construct(){
+		parent::__construct();
+		$this->_qqweibo = new qqweibo(C('QQ_T_KEY'), C('QQ_T_SECRET'), U('callback'));
+		$this->_moefm = new Moefou(C('MF_KEY'), C('MF_SECRET'), 'http://yukimax.cn/lab/mf/callback.php');
+	}
+	/**
+	 * 首页
+	 */
+	public function index(){
+		$this->display('indexv2');
+	}
+	/**
+	 * 萌否授权中转
+	 */
+	public function request_token(){
+		$this->_moefm->redirect_to_login();
+	}
+	/**
+	 * 获取token
+	 */
+	public function access_token(){
+		header('Content-Type:application/json;charset=utf-8');
+		$oauth_verifier = isset($_GET['oauth_verifier']) ? $_GET['oauth_verifier'] : null;
+		$str = $this->_moefm->get_access_token($_SESSION['oauth_token'], $_SESSION['oauth_token_secret'], $oauth_verifier);
+		$this->ajax(array('str' => $str));
+	}
+	/**
+	 * 检查登录状态
+	 */
+	public function check_login(){
+		header('Content-Type:application/json;charset=utf-8');
+		$oauth_token = isset($_GET['oauth_token']) ? $_GET['oauth_token'] : null;
+		$oauth_token_secret = isset($_GET['oauth_token_secret']) ? $_GET['oauth_token_secret'] : null;
+		$json = $this->_moefm->get_user_info($oauth_token, $oauth_token_secret);
+		unset($json['response']['information']);
+		$this->ajax($json);
+	}
+	/**
+	 * 萌否列表
+	 */
+	public function listen(){
+		header('Content-Type:application/json;charset=utf-8');
+		$oauth_token = isset($_GET['oauth_token']) ? $_GET['oauth_token'] : null;
+		$oauth_token_secret = isset($_GET['oauth_token_secret']) ? $_GET['oauth_token_secret'] : null;
+		unset($_GET['c']);
+		unset($_GET['m']);
+		unset($_GET['a']);
+		unset($_GET['_']);
+		unset($_GET['oauth_token']);
+		unset($_GET['oauth_token_secret']);
+		if ($oauth_token && $oauth_token_secret) {
+			$data = $this->_moefm->do_get('http://moe.fm/listen/playlist', $oauth_token, $oauth_token_secret);
+		} else {
+			$params = $this->_moefm->get_normalized_string($_GET);
+			$data = $this->_moefm->curl('http://moe.fm/listen/playlist?api_key='.C('MF_KEY').'&'.$params);
+		}
+		$json = json_decode($data, true);
+		unset($json['response']['information']['parameters']);
+		unset($json['response']['information']['request']);
+		$this->ajax($json);
+	}
+	/**
+	 * 收藏
+	 */
+	public function fav(){
+		header('Content-Type:application/json;charset=utf-8');
+		$oauth_token = isset($_GET['oauth_token']) ? $_GET['oauth_token'] : null;
+		$oauth_token_secret = isset($_GET['oauth_token_secret']) ? $_GET['oauth_token_secret'] : null;
+		$method = isset($_GET['method']) ? $_GET['method'] : null;
+		unset($_GET['c']);
+		unset($_GET['m']);
+		unset($_GET['a']);
+		unset($_GET['_']);
+		unset($_GET['method']);
+		unset($_GET['oauth_token']);
+		unset($_GET['oauth_token_secret']);
+		if ($oauth_token && $oauth_token_secret) {
+			$data = $this->_moefm->do_get('http://api.moefou.org/fav/'.$method.'.json', $oauth_token, $oauth_token_secret);
+		} else {
+			// $params = $this->_moefm->get_normalized_string($_GET);
+			// $data = $this->_moefm->curl('http://moe.fm/listen/playlist?api_key='.C('MF_KEY').'&'.$params);
+		}
+		$json = json_decode($data, true);
+		unset($json['response']['information']['parameters']);
+		unset($json['response']['information']['request']);
+		$this->ajax($json);
+	}
+	/**
+	 * 记录
+	 */
+	public function my_log(){
+		header('Content-Type:application/json;charset=utf-8');
+		$oauth_token = isset($_GET['oauth_token']) ? $_GET['oauth_token'] : null;
+		$oauth_token_secret = isset($_GET['oauth_token_secret']) ? $_GET['oauth_token_secret'] : null;
+		unset($_GET['c']);
+		unset($_GET['m']);
+		unset($_GET['a']);
+		unset($_GET['_']);
+		unset($_GET['method']);
+		unset($_GET['oauth_token']);
+		unset($_GET['oauth_token_secret']);
+		if ($oauth_token && $oauth_token_secret) {
+			$data = $this->_moefm->do_get('http://moe.fm/ajax/log', $oauth_token, $oauth_token_secret);
+		} else {
+			// $params = $this->_moefm->get_normalized_string($_GET);
+			// $data = $this->_moefm->curl('http://moe.fm/listen/playlist?api_key='.C('MF_KEY').'&'.$params);
+		}
+		$json = json_decode($data, true);
+		unset($json['response']['information']);
+		$this->ajax($json);
+	}
+	/**
+	 * 微博分享
+	 */
+	public function share(){
+		if(Rbac::isLogin()){
+			$user = M('user')->find(session('uid'));
+			if(!empty($user['qqweibo'])){
+				$qqweibo = unserialize($user['qqweibo']);
+				$data = $this->_qqweibo->add_pic_url('t/add_pic_url', $qqweibo['access_token'], $qqweibo['openid'], ip::getClientIp());exit();
+				$json = json_decode($data, true);
+				$this->ajax($qqweibo);
+			}
+		}else{
+			_404(__METHOD__.'页面没找到', '404.html');
+		}
+	}
 }
