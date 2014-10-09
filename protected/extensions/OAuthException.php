563dd11cd1f2fbc12d5d63e427d1002644e06dbc
nagatoyk <393117228@qq.com>
2014-10-09 18:55:19 +0800

    修正

---
+++ protected/extensions/OAuthException.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * PHP SDK for weibo.com (using OAuth2)
+ *
+ * @author Elmer Zhang <freeboy6716@gmail.com>
+ */
+
+/**
+ * @ignore
+ */
+class OAuthException extends Exception {
+    // pass
+}
