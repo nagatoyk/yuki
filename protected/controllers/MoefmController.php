ca0bb80bcb2097e6497603438ef10e6006c2f52f
nagatoyk <393117228@qq.com>
2014-10-09 19:52:05 +0800

    修正url

--- protected/controllers/MoefmController.php
+++ protected/controllers/MoefmController.php
@@ -7,14 +7,15 @@ class MoefmController extends Controller{
     }*/
     public function actionIndex(){
         $o = new SaeTOAuthV2(Yii::app()->params['saet_api_key'], Yii::app()->params['saet_api_secret']);
-        $this->render('index', array('wburl' => $o->getAuthorizeURL('http://i.loli-yuki.tk'.$this->createUrl('saetcallback'))));
+        $wburl = $o->getAuthorizeURL('http://i.loli-yuki.tk'.$this->createUrl('saetcallback'), 'code', 'moefm', 'mobile');
+        $this->render('index', array('wburl' => $wburl));
     }
     public function actionSaetcallback(){
         $o = new SaeTOAuthV2(Yii::app()->params['saet_api_key'], Yii::app()->params['saet_api_secret']);
         if(isset($_REQUEST['code'])){
             $keys = array();
             $keys['code'] = $_REQUEST['code'];
-            $keys['redirect_uri'] = $o->getAuthorizeURL('http://i.loli-yuki.tk'.$this->createUrl('saetcallback'));
+            $keys['redirect_uri'] = $o->getAuthorizeURL('http://i.loli-yuki.tk'.$this->createUrl('saetcallback'), 'code', 'moefm', 'mobile');
             try{
                 $token = $o->getAccessToken('code', $keys);
             }catch(OAuthException $e){
