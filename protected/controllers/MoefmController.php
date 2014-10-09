02230d5f8bda7a4d01e73e76a9e7fc4d1a66d595
nagatoyk <393117228@qq.com>
2014-10-09 18:58:51 +0800

    修正

--- protected/controllers/MoefmController.php
+++ protected/controllers/MoefmController.php
@@ -20,6 +20,7 @@ class MoefmController extends Controller{
             }catch(OAuthException $e){
             }
         }
+        var_dump($token);
         if($token){
             $_SESSION['token'] = $token;
             setcookie('weibojs_'.$o->client_id, http_build_query($token));
