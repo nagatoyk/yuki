160d8e17764ead57800c69b31f586ffdf09a9861
nagatoyk <393117228@qq.com>
2014-11-03 15:19:09 +0800

    修改

--- lab/callback.php
+++ lab/callback.php
@@ -4,15 +4,8 @@ require './config.php';
 require './saetv2.ex.class.php';
 $o = new SaeTOAuthV2(WB_AKEY, WB_SKEY);
 if(isset($_REQUEST['code'])){
-	switch($_REQUEST['state']){
-		case 'saeyuki':
-			header('Location:http://yukimax.sinaapp.com/site/callback?code='.$_REQUEST['code']);
-			exit();
-			break;
-
-		default:
-			header('Location:'.urldecode($_REQUEST['state']).'&code='.$_REQUEST['code']);
-			exit();
-			break;
+	if(isset($_REQUEST['state'])){
+		header('Location:'.urldecode($_REQUEST['state']).'&code='.$_REQUEST['code']);
+		exit();
 	}
 }
