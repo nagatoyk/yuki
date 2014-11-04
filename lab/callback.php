7a34711924950191327e8590223b7baad4b5c769
nagatoyk <393117228@qq.com>
2014-11-04 18:57:55 +0800

    SDFSADF

--- lab/callback.php
+++ lab/callback.php
@@ -3,8 +3,8 @@ session_start();
 require './config.php';
 require './saetv2.ex.class.php';
 $o = new SaeTOAuthV2(WB_AKEY, WB_SKEY);
-if(isset($_REQUEST['code'])){
-	if(isset($_REQUEST['state'])){
+if(isset($_GET['code'])){
+	if(isset($_GET['state'])){
 		header('Location:'.urldecode($_REQUEST['state']).'?code='.$_REQUEST['code']);
 		exit();
 	}else{
