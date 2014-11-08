74e60f238e543ebfe7762babf59c38ccd7eed2f3
nagatoyk <393117228@qq.com>
2014-11-08 12:42:34 +0800

    修改

--- lab/ip.php
+++ lab/ip.php
@@ -2,8 +2,7 @@
 require 'config.php';
 require 'saetv2.ex.class.php';
 if(isset($_COOKIE['weibojs_'.WB_AKEY])){
-	echo 123;
+	echo $_COOKIE['weibojs_'.WB_AKEY];
+	// $c = SaeTClient();
 }else{
-	echo 456;
-	setcookie('weibojs_'.WB_AKEY, 'access_token%3D2.00yc_LqBUTmzLBd034ebe359FIb5mB%26remind_in%3D157679999%26expires_in%3D157679999%26uid%3D1687199364', time() + 3600 * 24 *365);
 }
