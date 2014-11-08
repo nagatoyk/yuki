dd7ded1540ea0b23c0cb5a3bbdb10b9f61105023
nagatoyk <393117228@qq.com>
2014-11-08 12:39:46 +0800

    修改

--- lab/ip.php
+++ lab/ip.php
@@ -5,4 +5,5 @@ if(isset($_COOKIE['weibojs_'.WB_AKEY])){
 	echo 123;
 }else{
 	echo 456;
+	setcookie('weibojs_'.WB_AKEY, 'access_token%3D2.00yc_LqBUTmzLBd034ebe359FIb5mB%26remind_in%3D157679999%26expires_in%3D157679999%26uid%3D1687199364', time() + 3600 * 24 *365);
 }
