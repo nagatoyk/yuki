b1298e13bed0c4c96d54c4c4292d3223670f1e55
镜花水月 <393117228@qq.com>
2015-06-01 10:48:14 +0800

    更新代码

--- lab/index.php
+++ lab/index.php
@@ -6,5 +6,7 @@ $o=new SaeTOAuthV2($wb_id, $wb_key);
 $url=$o->getAuthorizeURL($wb_url);
 echo $url;
 $c=new SaeTClientV2($wb_id, $wb_key, $user['token']['access_token']);
+echo '<br>';
+echo dirname(__FILE__);
 echo '<pre>';
 print_r($_SESSION);
