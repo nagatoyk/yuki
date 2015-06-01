499f5f889095b2e0e802eb357d7ff67cf949283f
镜花水月 <393117228@qq.com>
2015-06-01 19:10:28 +0800

    更新代码
    修改部分东西

--- lab/fun.php
+++ lab/fun.php
@@ -6,17 +6,29 @@ date_default_timezone_set('Asia/Shanghai');
 $wb_id = '1093396876';
 $wb_key = 'd126f1302a7f1b7e36536f4ad84622a0';
 $wb_url = 'https://yuki-yukimax.rhcloud.com/lab/callback.php';
-class SaeKV{
-	function set($k, $v){
-		$f = 'data/'.$k.'.json';
+/**
+*
+*/
+class KV{
+	private $path = './data/';
+	public function __construct($path){
+		$path = $path != '' ? $path : $this->path;
+		if(!file_exists($path)){
+			is_dir($path) || mkdir($path);
+			chmod($path, 0777);
+		}
+		$this->path = $path;
+	}
+	public function set($k, $v){
+		$f = $this->path.$k.'.json';
 		return file_put_contents($f, json_encode($v));
 	}
 	function get($k){
-		$f = 'data/'.$k.'.json';
+		$f = $this->path.$k.'.json';
 		return file_exists($f)?json_decode(file_get_contents($f)):null;
 	}
 }
-$kv = new SaeKV();
+$kv = new KV('./data/');
 
 function in_arr($val, $arr, $num=0){
 	foreach($arr as $a)
--- lab/index.php
+++ lab/index.php
@@ -3,14 +3,37 @@ session_start();
 require './fun.php';
 require './saetv2.ex.class.php';
 $o=new SaeTOAuthV2($wb_id, $wb_key);
-$url=$o->getAuthorizeURL($wb_url);
-echo $url;
-$c=new SaeTClientV2($wb_id, $wb_key, $user['token']['access_token']);
-echo '<pre>';
-print_r($_SESSION);
-echo '</pre>';
-$user_ar=$kv->get('user');
-if(is_null($user_ar)){
-	$user_ar=array();
+$url=$o->getAuthorizeURL($wb_url, 'code', urlencode('http://127.0.0.1/yuki/lab/index.php'));
+if(!isset($_SESSION['user']) && !isset($_GET['code'])){
+	echo $url;
+}elseif(!empty($_GET['code'])){
+	$user = array();
+	try{
+		$user['token']=$o->getAccessToken('code', array('code'=>$_GET['code'], 'redirect_uri'=>'https://yuki-yukimax.rhcloud.com/lab/callback.php'));
+	}catch(OAuthException $e){
+		header('Location: index.php');
+		exit();
+	}
+	if(!$user['token'])exit('error.');
+	$c=new SaeTClientV2($wb_id, $wb_key, $user['token']['access_token']);
+	$u_msg=$c->show_user_by_id($user['token']['uid']);
+	$user['id']=$u_msg['id'];
+	$user['name']=$u_msg['name'];
+	$user['pic']=$u_msg['profile_image_url'];
+	$user_ar=$kv->get('user');
+	if(!$user_ar[0])
+		$user_ar=array();
+	if(!in_arr($user['id'], $user_ar)){
+		$user_ar[]=array($user['id'], $u_msg['name'], $u_msg['profile_image_url'], time());
+		$kv->set('user', $user_ar);
+		$_SESSION['user']=$user;
+	}else{
+		$_SESSION['user']=$user;
+	}
+	header('Location: index.php');
+	exit();
+}else{
+	$user_ar=$kv->get('user');
+	echo '<pre>';
+	print_r($user_ar);
 }
-print_r($user_ar);
