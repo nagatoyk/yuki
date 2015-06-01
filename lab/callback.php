ebe324498ca80549352e8332d4869f1183921f48
镜花水月 <393117228@qq.com>
2015-06-01 09:51:41 +0800

    更新代码

--- lab/callback.php
+++ lab/callback.php
@@ -2,13 +2,23 @@
 session_start();
 require './config.php';
 require './saetv2.ex.class.php';
-$o = new SaeTOAuthV2(WB_AKEY, WB_SKEY);
+$o = new SaeTOAuthV2($wb_id, $wb_key);
 if(isset($_GET['code'])){
 	if(isset($_GET['state'])){
-		header('Location:'.urldecode($_REQUEST['state']).'?code='.$_REQUEST['code']);
+		header('Location:'.urldecode($_GET['state']).'?code='.$_GET['code']);
 		exit();
 	}else{
-		echo 123;
+		$user = array();
+		try {
+			$user['token'] = $o->getAccessToken('code', array('code'=>$_GET['code'], 'redirect_uri'=>'https://yuki-yukimax.rhcloud.com/lab/callback.php'));
+		} catch (OAuthException $e) {
+			header('Location: /index.php');
+		}
+		if(!$user['token'])exit('error.');
+		$c = new SaeTClientV2($wb_id, $wb_key, $user['token']['access_token']);
+		$u_msg = $c->show_user_by_id($user['token']['uid']);
+		print_r($u_msg);
+
 	}
 }else{
 	echo 456;
--- lab/config.php
+++ lab/config.php
@@ -2,6 +2,6 @@
 // error_reporting(0);
 // header('Content-Type: text/html; charset=UTF-8');
 // 微博已上线站点api数据
-define("WB_AKEY", '1093396876');
-define("WB_SKEY", 'd126f1302a7f1b7e36536f4ad84622a0' );
-define("WB_CALLBACK_URL", 'https://yuki-yukimax.rhcloud.com/lab/callback.php');
+$wb_id = '1093396876';
+$wb_key = 'd126f1302a7f1b7e36536f4ad84622a0';
+$wb_url = 'https://yuki-yukimax.rhcloud.com/lab/callback.php';
