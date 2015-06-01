13ed4e02863d8c7ee63b79029f853d9e5c646528
镜花水月 <393117228@qq.com>
2015-06-01 11:26:00 +0800

    更新代码

--- lab/up.php
+++ lab/up.php
@@ -8,9 +8,7 @@ if(!empty($_FILES['img']['name'])){
 	// 上传路径
 	$path='uppic/';
 	// 检查是否有该文件夹，如果没有就创建，并给予最高权限
-	if(!file_exists($path)){
-		mkdir($path, 0700);
-	}
+	file_exists($path)||mkdir($path, 0700);
 	// 允许上传的文件格式
 	$tp=array('image/gif', 'image/pjpeg', 'image/jpeg'):
 	// 检查上传文件是否在允许上传的类型
@@ -19,6 +17,7 @@ if(!empty($_FILES['img']['name'])){
 		exit();
 	}
 	$filetype=$_FILES['img']['type'];
+	$type = '';
 	switch($filetype){
 		case 'image/jpeg':
 			$type='.jpg';
@@ -41,6 +40,7 @@ if(!empty($_FILES['img']['name'])){
 		// 图片名称
 		$img=$today.$type;
 		$flag=1;
+	}
 	if($flag){
 		// 特别注意这里传递给move_uploaded_file的第一个参数为上传到服务器上的临时文件
 		$result=move_uploaded_file($_FILES['img']['tmp_name'], $file2);
