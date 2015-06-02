9ee7d542e9955d2e608f112225c8f6e5ea61de7e
镜花水月 <393117228@qq.com>
2015-06-02 16:55:54 +0800

    冒泡

--- lab/index.php
+++ lab/index.php
@@ -2,38 +2,38 @@
 session_start();
 require './fun.php';
 require './saetv2.ex.class.php';
-$o=new SaeTOAuthV2($wb_id, $wb_key);
-$url=$o->getAuthorizeURL($wb_url, 'code', urlencode($_SERVER['HTTP_X_FORWARDED_PROTO'].'://'.($_SERVER['HTTP_HOST']=='127.0.0.1'?'127.0.0.1/yuki':$_SERVER['HTTP_HOST']).'/lab/index.php'));
+$o = new SaeTOAuthV2($wb_id, $wb_key);
+$url = $o->getAuthorizeURL($wb_url, 'code', urlencode($_SERVER['HTTP_X_FORWARDED_PROTO'].'://'.($_SERVER['HTTP_HOST'] == '127.0.0.1'?'127.0.0.1/yuki':$_SERVER['HTTP_HOST']).'/lab/index.php'));
 if(!isset($_SESSION['user']) && !isset($_GET['code'])){
 	echo $url;
 }elseif(!empty($_GET['code'])){
 	$user = array();
 	try{
-		$user['token']=$o->getAccessToken('code', array('code'=>$_GET['code'], 'redirect_uri'=>'https://yuki-yukimax.rhcloud.com/lab/callback.php'));
+		$user['token'] = $o->getAccessToken('code', array('code'=>$_GET['code'], 'redirect_uri'=>'https://yuki-yukimax.rhcloud.com/lab/callback.php'));
 	}catch(OAuthException $e){
 		header('Location: index.php');
 		exit();
 	}
 	if(!$user['token'])exit('error.');
-	$c=new SaeTClientV2($wb_id, $wb_key, $user['token']['access_token']);
-	$u_msg=$c->show_user_by_id($user['token']['uid']);
-	$user['id']=$u_msg['id'];
-	$user['name']=$u_msg['name'];
-	$user['pic']=$u_msg['profile_image_url'];
-	$user_ar=$kv->get('user');
+	$c = new SaeTClientV2($wb_id, $wb_key, $user['token']['access_token']);
+	$u_msg = $c->show_user_by_id($user['token']['uid']);
+	$user['id'] = $u_msg['id'];
+	$user['name'] = $u_msg['name'];
+	$user['pic'] = $u_msg['profile_image_url'];
+	$user_ar = $kv->get('user');
 	if(!$user_ar[0])
-		$user_ar=array();
+		$user_ar = array();
 	if(!in_arr($user['id'], $user_ar)){
-		$user_ar[]=array($user['id'], $u_msg['name'], $u_msg['profile_image_url'], time());
+		$user_ar[] = array($user['id'], $u_msg['name'], $u_msg['profile_image_url'], time());
 		$kv->set('user', $user_ar);
-		$_SESSION['user']=$user;
+		$_SESSION['user'] = $user;
 	}else{
-		$_SESSION['user']=$user;
+		$_SESSION['user'] = $user;
 	}
 	header('Location: index.php');
 	exit();
 }else{
-	$user_ar=$kv->get('user');
+	$user_ar = $kv->get('user');
 	echo getenv('OPENSHIFT_DATA_DIR');
 	echo '<pre>';
 	print_r($user_ar);
