7abfde7fb180cc5bee9f03b841c703885d4f7bd1
镜花水月 <393117228@qq.com>
2015-06-02 09:35:30 +0800

    调试
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/up.php
+++ lab/up.php
@@ -1,8 +1,45 @@
 <meta charset="utf-8">
+<script src="//yukimax.sinaapp.com/i/iTorr.m.js"></script>
 <form method="post" enctype="multipart/form-data">
 	<input type="file" name="img">
 	<input type="submit" value="上传">
 </form>
+<script>
+$.onsubmit=function(F,cb){
+	F.onsubmit=function(){
+		for(var i=0,l=F.length,o,r=[];i<l;i++){
+			o=F[i];
+			if(o.name){
+				if(o.required&&(
+					!o.value
+					||(o.type=='email'&&!o.value.match(/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/))
+					||(o.type=='url'&&!o.value.match(/^http[s]?:\/\/([\w-]+\.)+[\w-]+([\w-.\/?%&=]*)?$/))
+					)
+				){
+					alert('请输入正确的'+o.title);
+					o.focus();
+					return false;
+				}else if(o.type=='radio'){
+					if(o.checked==1)
+						r.push(o.name+'='+encodeURIComponent(o.value));
+				}else
+					r.push(o.name+'='+encodeURIComponent(o.value));
+			}
+
+		}
+		$.x(F.action,r.join('&'),cb)
+		return false
+	};
+};
+$.onsubmit($('form'), function(r) {
+	var xhr = new XMLHttpRequest();
+	if (xhr.upload) {
+		xhr.upload.onprogress = function(e) {
+			console.log('width:' + e.loaded / e.total * 100 + '%')
+		}
+	}
+});
+</script>
 <?php
 // 提取文件域内容名称，并判断
 if(!empty($_FILES['img']['name'])){
