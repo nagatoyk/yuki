dfb3faf920c7164998168aaeee4aa537c4eef689
镜花水月 <393117228@qq.com>
2015-06-03 09:02:06 +0800

    马跑
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-
+<?php
+defined('__ROOT__') || define('__ROOT__', dirname(__FILE__));
+require 'r/fun.php';
+require 'r/saetv2.ex.class.php';
+$o = new SaeTOAuthV2($wb_id, $wb_key);
+if(isset($_GET['login'])){
+	$url = $o->getAuthorizeURL($wb_url);
+	header('Location: '.$url);
+	exit();
+}
---
+++ login.php
@@ -0,0 +1,37 @@
+<?php
+require 'r/fun.php';
+if(isset($_GET['code'])){
+	require 'r/saetv2.ex.class.php';
+	$o = new SaeTOAuthV2($wb_id, $wb_key);
+	$user = array();
+	try {
+		$user['token'] = $o->getAccessToken('code', array('code'=>$_GET['code'],'redirect_uri'=>$wb_url));
+	} catch (OAuthException $e) {
+		header('Location: /');
+	}
+	if(!$user['token'])exit('error.');
+	$c = new SaeTClientV2($wb_id, $wb_key, $user['token']['access_token']);
+	$u_msg = $c->show_user_by_id($user['token']['uid']);
+	$user['id'] = $u_msg['id'];
+	$user['information'] = array(
+		$u_msg['id'],//'id'=>
+		$u_msg['name'],//'name'=>
+		$u_msg['description'],//'des'=>
+		$u_msg['profile_image_url'],//'avatar'=>
+		$u_msg['domain'],//'weibo'=>
+		$u_msg['url']//'website'=>
+	);
+	$user_ar = $sql->getData('SELECT uid,information FROM wb_user WHERE uid="'.$user['id'].'"');
+	if(!$user_ar[0]){
+		$sql->runSql('INSERT INTO wb_user (`unix`,`uid`,`information`) VALUES ('.time().','.$user['id'].',\''.addslashes(json_encode($user['information'])).'\')');
+	}
+	session_start();
+	$_SESSION['user']=$user;
+	$o_url = $_COOKIE['sty_url'];
+	if($o_url){
+		unset($_COOKIE['sty_url']);
+		header('Location: '.$o_url);
+	}else{
+		header('Location: /');
+	}
+}
