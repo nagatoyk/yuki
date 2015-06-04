25afbcf4e373511faebfe0bd6e0104431e3f9152
镜花水月 <393117228@qq.com>
2015-06-04 21:28:57 +0800

    2015年6月4日21:28:53
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/saveImg.php
+++ lab/saveImg.php
@@ -4,14 +4,17 @@ require '../r/Mysql.class.php';
 require 'saetv2.ex.class.php';
 if(!empty($_POST['imgOpt'])){
 	$imgurl = $_POST['imgOpt']['url'];
+	$pid = $_POST['imgOpt']['pid'];
 	$type = pathinfo($imgurl, PATHINFO_EXTENSION);
 	if(in_array($type, array('jpg', 'png', 'gif'))){
 		$my_token = $kv->get('my_token');
 		$token = $my_token['1687199364'];
+		$url = $sql->runSql('SELECT `url` FROM `wb_pic` WHERE `pid`=\''.$pid.'\'');
+		if($url)exit();
 		$c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
 		$msg = $c->upload('我刚刚上传了一张照片'.time(), $imgurl);
 		if(!isset($msg['error_code'])){
-			$sql->runSql('INSERT INTO wb_pic (`uid`,`url`,`unix`) VALUES (\''.$token['uid'].'\',\''.$msg['original_pic'].'\',\''.time().'\')');
+			$sql->runSql('INSERT INTO wb_pic (`uid`,`url`,`unix`,`pid`) VALUES (\''.$token['uid'].'\',\''.$msg['original_pic'].'\',\''.time().'\',\''.$pid.'\')');
 			$r['imgurl'] = $msg['original_pic'];
 			$c->delete($msg['id']);
 		}else{
