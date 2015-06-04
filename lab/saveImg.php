9917e080fbf703b381e80198567502dc27f3a241
镜花水月 <393117228@qq.com>
2015-06-04 20:04:54 +0800

    2015年6月4日20:04:41
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/saveImg.php
+++ lab/saveImg.php
@@ -1,16 +1,25 @@
 <?php
 require 'fun.php';
+require 'saetv2.ex.class.php';
 if(!empty($_POST['imgOpt'])){
 	foreach($_POST['imgOpt'] as $k => $v){
 		// echo
 	}
 	$imgurl = $_POST['imgOpt']['url'];
 	$r = $_POST['imgOpt'];
-	// $finfo = finfo_open(FILEINFO_MIME_TYPE);
-	// $type = finfo_file($finfo, $img);
-	// finfo_close($finfo);
 	$r['name'] = pathinfo($imgurl, PATHINFO_BASENAME);
 	$r['type'] = pathinfo($imgurl, PATHINFO_EXTENSION);
+	$my_token = $kv->get('my_token');
+	$token = $my_token[1687199364];
+	$c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
+	$msg = $c->upload('我刚刚上传了一张照片'.time(), $imgurl);
+	if($msg['original_pic']){
+		$sql->runSql('INSERT INTO wb_pic (`uid`,`url`,`unix`) VALUES (\''.$token['uid'].'\',\''.$msg['original_pic'].'\',\''.time().'\')');
+		$r['original'] = $msg['original_pic'];
+		$c->delete($msg['id']);
+	}else{
+		print_r($msg);
+	}
 	header('Content-Type: application/json;charset=utf-8');
 	header('Access-Control-Allow-Origin: *');
 	echo isset($_GET['cb']) ? $_GET['cb'].'('.json_encode($r).')' : json_encode($r);
