e97119e1ec4b3475439ca476e4cedbe1de85fb80
镜花水月 <393117228@qq.com>
2015-06-04 22:12:45 +0800

    调试 2015年6月4日22:12:42
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/saveImg.php
+++ lab/saveImg.php
@@ -9,8 +9,8 @@ if(!empty($_POST['imgOpt'])){
 	if(in_array($type, array('jpg', 'png', 'gif'))){
 		$my_token = $kv->get('my_token');
 		$token = $my_token['1687199364'];
-		$info = $sql->getLine('SELECT `url` FROM `wb_pic` WHERE `pid`=\''.$pid.'\'');
-		if(!isset($info['url'])){
+		$info = $sql->getLine('SELECT `pid` FROM `wb_pic` WHERE `pid`=\''.$pid.'\'');
+		if(!isset($info['pid'])){
 			$c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
 			$msg = $c->upload('我刚刚上传了一张照片'.time(), $imgurl);
 			if(!isset($msg['error_code'])){
