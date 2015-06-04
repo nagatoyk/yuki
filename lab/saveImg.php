d45b6b25deb0c4ba3fb20797bab6998423cf7ed8
镜花水月 <393117228@qq.com>
2015-06-04 17:21:25 +0800

    添加头
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/saveImg.php
+++ lab/saveImg.php
@@ -3,5 +3,7 @@ if(!empty($_POST['imgOpt'])){
 	foreach($_POST['imgOpt'] as $k => $v){
 		// echo
 	}
+	header('Content-Type: application/json;charset=utf-8');
+	header('Access-Control-Allow-Origin: *');
 	echo isset($_GET['cb']) ? $_GET['cb'].'('.json_encode($_POST['imgOpt']).')' : json_encode($_POST['imgOpt']);
 }
