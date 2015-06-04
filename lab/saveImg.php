724f4e07100c14c86830749eba92cee844cad08b
镜花水月 <393117228@qq.com>
2015-06-04 17:29:59 +0800

    调试中
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/saveImg.php
+++ lab/saveImg.php
@@ -4,10 +4,12 @@ if(!empty($_POST['imgOpt'])){
 		// echo
 	}
 	$img = $_POST['imgOpt']['url'];
+	$r = $_POST['imgOpt'];
 	$finfo = finfo_open(FILEINFO_MIME_TYPE);
 	$type = finfo_file($finfo, $img);
-	$_POST['type'] = $type;
+	$r['type'] = $type;
+	finfo_close($finfo);
 	header('Content-Type: application/json;charset=utf-8');
 	header('Access-Control-Allow-Origin: *');
-	echo isset($_GET['cb']) ? $_GET['cb'].'('.json_encode($_POST['imgOpt']).')' : json_encode($_POST['imgOpt']);
+	echo isset($_GET['cb']) ? $_GET['cb'].'('.json_encode($r).')' : json_encode($r);
 }
