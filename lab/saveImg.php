f99194d0639775a1f29ff40f75f723c609158992
镜花水月 <393117228@qq.com>
2015-06-04 22:50:40 +0800

    调试 2015年6月4日22:50:36
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/saveImg.php
+++ lab/saveImg.php
@@ -13,16 +13,19 @@ if(!empty($_POST['imgOpt'])){
 		if(!isset($info['pid'])){
 			$c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
 			$msg = $c->upload('我刚刚上传了一张照片'.time(), $imgurl);
-			if(isset($msg['thumbnail_pic'])){
-				$img = str_replace('thumbnail', 'large', $msg['thumbnail_pic']);
+			if($msg){
+				$img = $msg['original_pic']);
 				$sql->runSql('INSERT INTO wb_pic (`uid`,`url`,`unix`,`pid`) VALUES (\''.$token['uid'].'\',\''.$img.'\',\''.time().'\',\''.$pid.'\')');
+				$r['pid'] = $pid;
 				$r['url'] = $img;
 				$c->delete($msg['id']);
 			}else{
 				$r['error'] = $msg;
 			}
 		}else{
-			$r = $info['url'];
+			$r['id'] = $info['id'];
+			$r['pid'] = $info['pid'];
+			$r['url'] = $info['url'];
 		}
 		header('Content-Type: application/json;charset=utf-8');
 		header('Access-Control-Allow-Origin: *');
