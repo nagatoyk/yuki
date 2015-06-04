15ed45c6068e54e5feb46e4a00711a1af15f1102
镜花水月 <393117228@qq.com>
2015-06-04 17:33:14 +0800

    调试中...
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/saveImg.php
+++ lab/saveImg.php
@@ -3,12 +3,12 @@ if(!empty($_POST['imgOpt'])){
 	foreach($_POST['imgOpt'] as $k => $v){
 		// echo
 	}
-	$img = $_POST['imgOpt']['url'];
+	$imgurl = $_POST['imgOpt']['url'];
 	$r = $_POST['imgOpt'];
-	$finfo = finfo_open(FILEINFO_MIME_TYPE);
-	$type = finfo_file($finfo, $img);
-	$r['type'] = $type;
-	finfo_close($finfo);
+	// $finfo = finfo_open(FILEINFO_MIME_TYPE);
+	// $type = finfo_file($finfo, $img);
+	// finfo_close($finfo);
+	$r['type'] = pathinfo($imgurl, PATHINFO_EXTENSION);
 	header('Content-Type: application/json;charset=utf-8');
 	header('Access-Control-Allow-Origin: *');
 	echo isset($_GET['cb']) ? $_GET['cb'].'('.json_encode($r).')' : json_encode($r);
