a888a0391409463b3adb971b0fdfd356cc8df9f6
镜花水月 <393117228@qq.com>
2015-06-05 17:41:04 +0800

    2015年6月5日17:41:01
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/saveImg.php
+++ lab/saveImg.php
@@ -4,10 +4,9 @@ require '../r/Mysql.class.php';
 require '../r/saetv2.ex.class.php';
 if(!empty($_POST['imgOpt'])){
 	$url = urldecode($_POST['imgOpt']['url']);
-	$host = parse_url($url);
 	$dir = pathinfo($url, PATHINFO_DIRNAME);
 	$filename = pathinfo($url, PATHINFO_BASENAME);
-	$imgurl = $host['scheme'].'://'.$host['host'].'/'.$dir.'/'.$filename;
+	$imgurl = $dir.'/'.$filename;
 	$pid = $_GET['pid'];
 	$type = pathinfo($imgurl, PATHINFO_EXTENSION);
 	if(in_array($type, array('jpg', 'png', 'gif'))){
