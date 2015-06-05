077612e7b63fcf493785e9409e3b99411c597bad
镜花水月 <393117228@qq.com>
2015-06-05 14:51:28 +0800

    修改判断方式 2015年6月5日14:51:25
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- lab/saveImg.php
+++ lab/saveImg.php
@@ -13,7 +13,7 @@ if(!empty($_POST['imgOpt'])){
 		if(!isset($info['pid'])){
 			$c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
 			$msg = $c->upload('我刚刚上传了一张照片'.time(), $imgurl);
-			if($msg !== NULL){
+			if(!empty($msg)){
 				if($msg['original_pic']){
 					$img = $msg['original_pic'];
 					$sql->runSql('INSERT INTO wb_pic (`uid`,`url`,`unix`,`pid`) VALUES (\''.$token['uid'].'\',\''.$img.'\',\''.time().'\',\''.$pid.'\')');
