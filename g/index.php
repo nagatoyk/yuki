e5ef16bfde69bfec4f265b583b4a7a8cf846a699
镜花水月 <393117228@qq.com>
2015-06-05 22:48:24 +0800

    调试 2015年6月5日22:48:21
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- g/index.php
+++ g/index.php
@@ -3,9 +3,6 @@ require '../r/fun.php';
 require '../r/Mysql.class.php';
 require '../r/saetv2.ex.class.php';
 $data = $sql->getData('SELECT `url` AS `imgurl`,FROM_UNIXTIME(unix, \'%Y-%m-%d %H:%i:%s\') AS time FROM `wb_pic` ORDER BY `unix` DESC');
-echo '<pre>';
-print_r($data);
-echo '</pre>';
 ?>
 <!DOCTYPE html>
 <html>
@@ -14,8 +11,10 @@ echo '</pre>';
 	<title>图片管理</title>
 </head>
 <body>
-<ul>
-	<li><img src="<?php echo $imgurl; ?>"></li>
-</ul>
+<table>
+	<?php foreach($data as $k => $v): ?>
+	<tr><td><img src="<?php echo str_replace('large', 'thumb150', $v['imgurl']); ?>"></td><td><?php echo $v['time']; ?></td></tr>
+	<?php endforeach; ?>
+</table>
 </body>
 </html>
