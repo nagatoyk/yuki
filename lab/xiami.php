20c2f9404efdcc8725b532e833867133ecf71010
镜花水月 <393117228@qq.com>
2015-06-07 14:55:49 +0800

    调试 2015年6月7日14:55:46
    Signed-off-by: 镜花水月 <393117228@qq.com>

---
+++ lab/xiami.php
@@ -0,0 +1,36 @@
+<?php
+require '../r/fun.php';
+require '../r/saetv2.ex.class.php';
+$o = new SaeTOAuthV2($wb_id, $wb_key);
+if(isset($_GET['code'])){
+	$code = $_GET['code'];
+	$user = array();
+	try{
+		$user['token'] = $o->getAccessToken('code', array('code' => $_GET['code'], 'redirect_uri'=>'https://yuki-yukimax.rhcloud.com/lab/xiami.php'));
+		session_start();
+		$_SESSION['user'] = $user;
+		header('Location: /lab/xiami.php?vcode='.$user['token']['access_tokne']);
+		exit();
+	}catch(OAuthException $e){
+		header('Location: /lab/err.php?e='.json_encode($e));
+		exit();
+	}
+}elseif(isset($_GET['login'])){
+	header('Location: '.$o->getAuthorizeURL('https://yuki-yukimax.rhcloud.com/lab/xiami.php');
+	exit();
+}
+?>
+<!doctype html>
+<meta charset="utf-8">
+<script>
+if(window.location.href.indexOf('vcode') >= 0){
+	var vcode = window.location.href.match(/vcode=(\w{1,})/)[1];
+	if(window.opener){
+		window.opener.localStorage.accessToken = vcode;
+		window.close()
+	}else{
+		prompt('请复制下面的验证码并贴入播放页指定文本框以完成验证', vcode);
+		window.close()
+	}
+}
+</script>
