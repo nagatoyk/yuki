17bd2cf6d1546297c4b2a6e18211c48a1fda60a9
镜花水月 <393117228@qq.com>
2015-06-20 00:34:42 +0800

    修改
    修改

--- p/saveImg.php
+++ p/saveImg.php
@@ -7,17 +7,18 @@ if(isset($_POST['imgOpt'])){
 	$pid = sprintf('%d', $_GET['pid']);
 	$type = pathinfo($url, PATHINFO_EXTENSION);
 	if(in_array($type, array('jpg', 'png', 'gif'))){
+		$r = array();
 		$my_token = $kv->get('my_token');
 		$token = $my_token[1687199364];
-		$info = $sql->getLine('SELECT * FROM `wb_pic` WHERE `pid`=\''.$pid.'\'');
+		$info = $sql->getLine("SELECT * FROM `wb_pic` WHERE `pid`='{$pid}'");
 		if(!isset($info['pid'])){
 			$c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
 			$c->upload('我刚刚上传了一张照片---'.$pid.'------'.time(), $url);
 			$u = $c->user_timeline_by_id($token['uid'], 1, 1);
 			if($u['statuses']){
 				if(strpos($u['statuses'][0]['text'], $pid) !== false){
-					$sql->runSql('INSERT INTO `wb_pic` (`uid`,`url`,`unix`,`pid`,`source`) VALUES (\''.$token['uid'].'\',\''.$u['statuses'][0]['original_pic'].'\',UNIX_TIMESTAMP(),\''.$pid.'\',\''.$_POST['imgOpt']['source'].'\')');
-					$m = $sql->getLine('SELECT * FROM `wb_pic` WHERE `url`=\''.$u['statuses'][0]['original_pic'].'\'');
+					$sql->runSql("INSERT INTO `wb_pic` (`uid`,`url`,`unix`,`pid`,`source`) VALUES ('{$token['uid']}','{$u['statuses'][0]['original_pic']}',UNIX_TIMESTAMP(),'{$pid}','{$_POST['imgOpt']['source']}')");
+					$m = $sql->getLine("SELECT * FROM `wb_pic` WHERE `url`='{$u['statuses'][0]['original_pic']}'");
 					if(isset($m['pid'])){
 						$r = $m;
 					}else{
