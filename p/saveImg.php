b9e7f61515921b3ab8896c0d8c9e761c86236f7b
镜花水月 <393117228@qq.com>
2015-06-19 15:08:05 +0800

    修改
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- p/saveImg.php
+++ p/saveImg.php
@@ -4,17 +4,18 @@ require '../r/Mysql.class.php';
 require '../r/saetv2.ex.class.php';
 if(isset($_POST['imgOpt'])){
 	$url = $_POST['imgOpt']['url'];
+	$pid = intval($_GET['pid']);
 	$type = pathinfo($url, PATHINFO_EXTENSION);
 	if(in_array($type, array('jpg', 'png', 'gif'))){
 		$my_token = $kv->get('my_token');
 		$token = $my_token[1687199364];
-		$info = $sql->getLine('SELECT * FROM `wb_pic` WHERE `pid`=\''.(int)$_GET['pid'].'\'');
+		$info = $sql->getLine('SELECT * FROM `wb_pic` WHERE `pid`=\''.$pid.'\'');
 		if(!isset($info['pid'])){
 			$c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
-			$msg = $c->upload('我刚刚上传了一张照片---'.(int)$_GET['pid'].'------'.time(), $url);
+			$msg = $c->upload('我刚刚上传了一张照片---'.$pid.'------'.time(), $url);
 			if($msg['original_pic']){
 				$img = $msg['original_pic'];
-				$sql->runSql('INSERT INTO wb_pic (`uid`,`url`,`unix`,`pid`,`source`) VALUES (\'1687199364\',\''.$img.'\',\''.time().'\',\''.(int)$_GET['pid'].'\',\''.$_POST['imgOpt']['source'].'\')');
+				$sql->runSql('INSERT INTO wb_pic (`uid`,`url`,`unix`,`pid`,`source`) VALUES (\'1687199364\',\''.$img.'\',\''.time().'\',\''.$pid.'\',\''.$_POST['imgOpt']['source'].'\')');
 				$c->delete($msg['id']);
 				$r = $msg;
 			}else{
@@ -22,7 +23,7 @@ if(isset($_POST['imgOpt'])){
 				if($u['statuses']){
 					$info = $sql->getLine('SELECT * FROM `wb_pic` WHERE `url`=\''.$u['statuses'][0]['original_pic'].'\'');
 					if(!isset($info['url'])){
-						$sql->runSql('INSERT INTO wb_pic (`uid`,`url`,`unix`,`pid`,`source`) VALUES (\'1687199364\',\''.$u['statuses'][0]['original_pic'].'\',\''.time().'\',\''.(int)$_GET['pid'].'\',\''.$_POST['imgOpt']['source'].'\')');
+						$sql->runSql('INSERT INTO wb_pic (`uid`,`url`,`unix`,`pid`,`source`) VALUES (\'1687199364\',\''.$u['statuses'][0]['original_pic'].'\',\''.time().'\',\''.$pid.'\',\''.$_POST['imgOpt']['source'].'\')');
 						$r = $sql->getLine('SELECT * FROM `wb_pic` WHERE `url`=\''.$u['statuses'][0]['original_pic'].'\'');
 						$c->delete($u['statuses'][0]['id']);
 					}else{
