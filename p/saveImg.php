1ebb6f4e0fcb51f3335311693b8c80acf87d165e
镜花水月 <393117228@qq.com>
2015-06-20 10:03:04 +0800

    修改
    Signed-off-by: 镜花水月 <393117228@qq.com>

--- p/saveImg.php
+++ p/saveImg.php
@@ -10,7 +10,7 @@ if(isset($_POST['imgOpt'])){
 		$r = array();
 		$my_token = $kv->get('my_token');
 		$token = $my_token[1687199364];
-		$pid = $sql->getLine("SELECT `pid` FROM `wb_pic` WHERE `pid`='{$pid}'");
+		$pid = $sql->getLine("SELECT `pid` FROM `wb_pic` WHERE `pid`='$pid'");
 		$c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
 		if(!isset($pid['pid'])){
 			$c->upload('我刚刚上传了一张照片---'.$pid.'------'.time(), $url);
