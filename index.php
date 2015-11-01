8367451ee422df5f57487202c072d71508422826
nagatoyk <393117228@qq.com>
2015-11-01 10:42:22 +0800

    修改
    Signed-off-by: nagatoyk <393117228@qq.com>

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $wb_key = 'd126f1302a7f1b7e36536f4ad84622a0';
 if(isset($_GET['upload']) && isset($_POST['file'])){
 	session_start();
 	echo '<pre>';
-	$c = new SaeTClientV2($wb_id, $wb_key, $_POST['file']['token']);
+	$c = new SaeTClientV2('1093396876', 'd126f1302a7f1b7e36536f4ad84622a0', $_POST['file']['token']);
 	$res = $c->upload('我刚刚上传了一张照片'.time(), $_POST['file']['image']);
 	print_r($res);
 }
