b767301eb641456488ec557ace535e43e3fa8276
nagatoyk <393117228@qq.com>
2015-11-01 10:59:40 +0800

    修改
    Signed-off-by: nagatoyk <393117228@qq.com>

--- index.php
+++ index.php
@@ -7,10 +7,10 @@
 	<h1>准备中</h1>
 	<?php echo time(); ?>
 	<form action="?upload" method="post">
-		<p><input type="text" name="file[token]"></p>
-		<p><input type="text" name="file[filename]" value="<?php echo time(); ?>.jpg"></p>
-		<p><textarea name="file[image]"></textarea></p>
-		<p><input type="submit" value="提交"></p>
+		<p><input type="text" name="filename" value="<?php echo time(); ?>.jpg"></p>
+		<p><textarea name="image"></textarea></p>
+		<input type="hidden" name="token" value="2.00yc_LqBLaBBXB73a1204f1e08OWQX">
+		<p><input type="submit" name="submit" value="提交"></p>
 	</form>
 </body>
 </html>
@@ -18,9 +18,10 @@
 require 'r/saetv2.ex.class.php';
 $wb_id = '1093396876';
 $wb_key = 'd126f1302a7f1b7e36536f4ad84622a0';
-if(isset($_GET['upload']) && isset($_POST['file'])){
+if(isset($_GET['upload']) && isset($_POST['submit'])){
 	session_start();
 	echo '<pre>';
+	print_r($_POST);
 	$c = new SaeTClientV2('1093396876', 'd126f1302a7f1b7e36536f4ad84622a0', $_POST['file']['token']);
 	$res = $c->upload('我刚刚上传了一张照片'.time(), $_POST['file']['image']);
 	print_r($res);
