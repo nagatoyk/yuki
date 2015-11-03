dc805e3ffae584cbfe6e92d8890cc6d81a00f847
nagatoyk <393117228@qq.com>
2015-11-03 19:57:20 +0800

    修改 微博 发布简介
    Signed-off-by: nagatoyk <393117228@qq.com>

--- saveImg.php
+++ saveImg.php
@@ -16,7 +16,7 @@ if(isset($_POST['image'])&&!empty($_POST['image'])){
 	file_put_contents($filename, $data);
 	$c = new SaeTClientV2($wb_id, $wb_key, $_POST['token']);
 
-	$res = $c->upload('#艦隊これくしょん# 玩家截图 发布于 '.date('Y-m-d H:i:s'), 'https://yuki-yukimax.rhcloud.com/'.$filename);
+	$res = $c->upload('#艦隊これくしょん# 玩家截图 发布于 '.date('Y-m-d H:i:s').' 经由<KC3改>插件截图发布,插件作者: @凉宫长门', 'https://yuki-yukimax.rhcloud.com/'.$filename);
 
 	if($res['original_pic'] && $_POST['from'] == 'Kancolle'){
 		$res['url'] = $res['original_pic'];
