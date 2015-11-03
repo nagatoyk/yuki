81358d29217086c5cabcef1fe659d06d2feb2218
nagatoyk <393117228@qq.com>
2015-11-03 23:00:13 +0800

    修改
    Signed-off-by: nagatoyk <393117228@qq.com>

--- index.php
+++ index.php
@@ -41,11 +41,11 @@ foreach(glob('{,.}*', GLOB_BRACE) as $filename){
 	// echo '<p>'.$filename.'</p>';
 }
 echo '</p><p>';
-foreach(glob('files/images/*') as $filename){
+foreach(glob('files/images/*.(jpg|jpg|png|bmp|gif)') as $filename){
 	echo '<a href="/'.$filename.'" target="_blank"><img src="'.$filename.'" width="300px" height="auto"></a> -- <a href="?delete='.$filename.'">删除</a>';
 }
 echo '</p><p>';
-foreach(glob('files/images/Kancolle/*') as $filename){
+foreach(glob('files/images/Kancolle/*.(jpg|jpg|png|bmp|gif)') as $filename){
 	echo '<a href="/'.$filename.'" target="_blank"><img src="'.$filename.'" width="300px" height="auto"></a> -- <a href="?delete='.$filename.'">删除</a>';
 }
 echo '</p><p>';
