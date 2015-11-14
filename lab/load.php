2fa1ced5e8f64f430488f2c28f94af84850f8edf
nagatoyk <393117228@qq.com>
2015-11-15 03:31:53 +0800

    修改 load.php
    Signed-off-by: nagatoyk <393117228@qq.com>

--- lab/load.php
+++ lab/load.php
@@ -14,5 +14,6 @@ if(isset($_GET)){
 	}
 	$url = 'http://bits.moegirl.org/zh/load.php?'.$_SERVER['QUERY_STRING'];
 	$file = file_get_contents($url);
+	$file = str_replace('//bits.moegirl.org/zh/load.php', '/lab/load.php', $file);
 	echo $file;
 }
