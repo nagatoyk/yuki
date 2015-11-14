ff91362aff0f4bac864374be71e93d4abc75fe8a
nagatoyk <393117228@qq.com>
2015-11-14 17:29:18 +0800

    修改

--- lab/browse.php
+++ lab/browse.php
@@ -20,16 +20,17 @@ $getIP = new getIP();
 echo $getIP::serverIP();
 if(!empty($_POST['sub'])){
 	$ch = curl_init();
-	curl_setopt($ch, CURLOPT_URL, $_POST['url']);
-	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0');
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 	curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC); //代理认证模式
 	curl_setopt($ch, CURLOPT_PROXY, 'proxy.kancolle.tv'); //代理服务器地址
 	curl_setopt($ch, CURLOPT_PROXYPORT, 9012); //代理服务器端口
-	//curl_setopt($ch, CURLOPT_PROXYUSERPWD, ":"); //http代理认证帐号，username:password的格式
-	curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //使用http代理模式
+	// curl_setopt($ch, CURLOPT_PROXYUSERPWD, ":"); //http代理认证帐号，username:password的格式
+	// curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //使用http代理模式
+	curl_setopt($ch, CURLOPT_URL, $_POST['url']);
+	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0');
+	curl_setopt ($ch, CURLOPT_HEADER, 1);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
 	$file_contents = curl_exec($ch);
 	curl_close($ch);
 	echo $file_contents;
