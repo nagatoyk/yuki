1cfab1a99ef838675765c301222822b2de264a7e
nagatoyk <393117228@qq.com>
2015-11-14 17:35:23 +0800

    修改

--- lab/browse.php
+++ lab/browse.php
@@ -24,7 +24,7 @@ if(!empty($_POST['sub'])){
 	curl_setopt($ch, CURLOPT_PROXY, '118.170.39.84'); //代理服务器地址
 	curl_setopt($ch, CURLOPT_PROXYPORT, 8888); //代理服务器端口
 	// curl_setopt($ch, CURLOPT_PROXYUSERPWD, ":"); //http代理认证帐号，username:password的格式
-	// curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //使用http代理模式
+	curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //使用http代理模式
 	curl_setopt($ch, CURLOPT_URL, $_POST['url']);
 	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0');
 	curl_setopt ($ch, CURLOPT_HEADER, 1);
