8246f7d352fafe96063502c8543f98f66b6a3a40
nagatoyk <393117228@qq.com>
2015-11-15 03:21:01 +0800

    修改 load.php
    Signed-off-by: nagatoyk <393117228@qq.com>

--- lab/load.php
+++ lab/load.php
@@ -1,7 +1,16 @@
 <?php
 if(isset($_GET)){
-	if(isset($_GET['only']) && $_GET['only'] == 'styles'){
-		header('Content-Type:text/css;charset=utf-8');
+	if(isset($_GET['only'])) require false;
+	switch($_GET['only']){
+		case 'styles':
+			header('Content-Type:text/css;charset=utf-8');
+			break;
+		case 'scripts':
+			header('Content-Type:application/javascript;charset=utf-8');
+			break;
+		default:
+			# code...
+			break;
 	}
 	$url = 'http://bits.moegirl.org/zh/load.php?'.$_SERVER['QUERY_STRING'];
 	$file = file_get_contents($url);
