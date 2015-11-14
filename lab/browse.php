9f8427d47a4fce04ac1bd716438a2030c15825c3
nagatoyk <393117228@qq.com>
2015-11-14 17:33:12 +0800

    修改代理地址

--- lab/browse.php
+++ lab/browse.php
@@ -21,8 +21,8 @@ echo $getIP::serverIP();
 if(!empty($_POST['sub'])){
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC); //代理认证模式
-	curl_setopt($ch, CURLOPT_PROXY, 'proxy.kancolle.tv'); //代理服务器地址
-	curl_setopt($ch, CURLOPT_PROXYPORT, 9012); //代理服务器端口
+	curl_setopt($ch, CURLOPT_PROXY, '118.170.39.84'); //代理服务器地址
+	curl_setopt($ch, CURLOPT_PROXYPORT, 8888); //代理服务器端口
 	// curl_setopt($ch, CURLOPT_PROXYUSERPWD, ":"); //http代理认证帐号，username:password的格式
 	// curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //使用http代理模式
 	curl_setopt($ch, CURLOPT_URL, $_POST['url']);
