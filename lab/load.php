26eb21b07d3c7887a70fcce7c94f33f786b27930
nagatoyk <393117228@qq.com>
2015-11-15 03:09:44 +0800

    修改 load.php
    Signed-off-by: nagatoyk <393117228@qq.com>

--- lab/load.php
+++ lab/load.php
@@ -1,7 +1,6 @@
 <?php
-$get = isset($_GET['l']) ? $_GET['l'] : null;
-if($get != null){
-	$url = 'http://bits.moegirl.org/zh/load.php?'.$;
+if(isset($_GET)){
+	$url = 'http://bits.moegirl.org/zh/load.php?'.$_SERVER['QUERY_STRING'];
 	$file = file_get_contents($url);
 	echo $file;
 }
