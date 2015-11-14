fa03e2c9f403b3747cb2599c8b68d2745b49657f
nagatoyk <393117228@qq.com>
2015-11-15 03:14:32 +0800

    修改 load.php
    Signed-off-by: nagatoyk <393117228@qq.com>

--- lab/load.php
+++ lab/load.php
@@ -1,5 +1,8 @@
 <?php
 if(isset($_GET)){
+	if(isset($_GET['only']) && $_GET['only'] == 'styles'){
+		header('Content-Type:text/css;charset=utf-8');
+	}
 	$url = 'http://bits.moegirl.org/zh/load.php?'.$_SERVER['QUERY_STRING'];
 	$file = file_get_contents($url);
 	echo $file;
