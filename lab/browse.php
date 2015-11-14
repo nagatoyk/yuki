8b1d15f2bc882b91c4da492434e88a82e62cd6b2
nagatoyk <393117228@qq.com>
2015-11-14 19:13:42 +0800

    修改

--- lab/browse.php
+++ lab/browse.php
@@ -22,9 +22,9 @@ if(!empty($_POST['sub'])){
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_BASIC); //代理认证模式
 	// curl_setopt($ch, CURLOPT_PROXY, 'proxy.kancolle.tv'); //代理服务器地址
-	curl_setopt($ch, CURLOPT_PROXY, '112.219.150.147');
+	curl_setopt($ch, CURLOPT_PROXY, '212.126.112.142');
 	// curl_setopt($ch, CURLOPT_PROXYPORT, 9012); //代理服务器端口
-	curl_setopt($ch, CURLOPT_PROXYPORT, 3128);
+	curl_setopt($ch, CURLOPT_PROXYPORT, 8080);
 	// curl_setopt($ch, CURLOPT_PROXYUSERPWD, ":"); //http代理认证帐号，username:password的格式
 	curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_HTTP); //使用http代理模式
 	curl_setopt($ch, CURLOPT_URL, $_POST['url']);
