68d71c8a22f8b4c197b16cfed3d09fdd2dbf7f8e
nagatoyk <393117228@qq.com>
2015-11-15 03:39:35 +0800

    修改 browse.php 添加 get.php
    Signed-off-by: nagatoyk <393117228@qq.com>

--- lab/browse.php
+++ lab/browse.php
@@ -37,6 +37,7 @@ if(!empty($_POST['sub'])){
 	curl_close($ch);
 	// $file_contents = preg_replace('/<title>(.*?)<\/title>/', "<title>$1</title>\n\r<base href=\"http://zh.moegirl.org/\">", $file_contents);
 
+	$file_contents = str_replace('/index.php', '/lab/get.php', $file_contents);
 	$file_contents = str_replace('//bits.moegirl.org/zh/load.php', '/lab/load.php', $file_contents);
 
 	preg_match_all('/^href=\"(\/\/bits\.moegirl\.org\/zh\/load\.php.*?)\"$/', $file_contents, $mat);
