a3f252e958e170fea1ebc349897157dffffe99a9
凉宫长门 <393117228@qq.com>
2016-04-13 18:25:43 +0800

    修改登录返回地址
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- r/fun.php
+++ r/fun.php
@@ -1,7 +1,7 @@
 <?php
 $wb_id = '1093396876';
 $wb_key = 'd126f1302a7f1b7e36536f4ad84622a0';
-$wb_url = 'https://yuki-yukimax.rhcloud.com/login.php';
+$wb_url = 'http://kloli.tk/login.php';
 class KV{
 	public function set($k, $v){
 		$f = '../data/'.$k.'.json';
