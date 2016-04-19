0c8589f4af1028fb92f40785cfd483520dd89591
凉宫长门 <393117228@qq.com>
2016-04-19 13:37:03 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- r/x.am.php
+++ r/x.am.php
@@ -17,10 +17,10 @@ function getLen($id){
 }
 
 preg_match('/http\:\/\/www\.xiami\.com\/album\/([0-9]{5,12})/s', $_POST['url'], $u);
+print_r($u);
 // $f = new SaeFetchurl();
 // $file = $f->fetch($u[0]);
 $file = file_get_contents($u[0]);
-echo $file;
 preg_match_all('/<(table)[^>]*class="track_list">(.*?)<\/\\1>/is', $file, $tbody);
 
 preg_match_all('/<td[^>]*class="song_name">[^>]*<a[^>]*href="\/song\/([\d]+)"/is', $tbody[2][0], $data);
