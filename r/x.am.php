a8dc7f77fb0300e886ba02b1591c2e814b32a95e
凉宫长门 <393117228@qq.com>
2016-04-19 13:06:14 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- r/x.am.php
+++ r/x.am.php
@@ -3,20 +3,23 @@ if(!isset($_POST['url']) && !preg_match('/http\:\/\/www\.xiami\.com\/album\/[0-9
 	err('参数错误');
 function getName($id){
 	$url = 'http://m.xiami.com/song/playlist/id/'.$id.'/object_name/default/object_id/0/cat/json?_='.time();
-	$f = new SaeFetchurl();
-	$json = json_decode($f->fetch($url), true);
+	// $f = new SaeFetchurl();
+	// $json = json_decode($f->fetch($url), true);
+	$json = json_decode(file_get_contents($url), true);
 	return $json['data']['trackList'][0]['title'];
 }
 function getLen($id){
 	$url = 'http://m.xiami.com/song/playlist/id/'.$id.'/object_name/default/object_id/0/cat/json?_='.time();
-	$f = new SaeFetchurl();
-	$json = json_decode($f->fetch($url), true);
+	// $f = new SaeFetchurl();
+	// $json = json_decode($f->fetch($url), true);
+	$json = json_decode(file_get_contents($url), true);
 	return $json['data']['trackList'][0]['length'];
 }
 
 preg_match('/http\:\/\/www\.xiami\.com\/album\/([0-9]{5,12})/s', $_POST['url'], $u);
-$f = new SaeFetchurl();
-$file = $f->fetch($u[0]);
+// $f = new SaeFetchurl();
+// $file = $f->fetch($u[0]);
+$file = file_get_contents($u[0]);
 preg_match_all('/<(table)[^>]*class="track_list">(.*?)<\/\\1>/is', $file, $tbody);
 
 preg_match_all('/<td[^>]*class="song_name">[^>]*<a[^>]*href="\/song\/([0-9]{5,12})"/is', $tbody[2][0], $data);
@@ -36,19 +39,19 @@ for($i = 0; $i < count($data[1]); $i++){
 	}
 }
 
-$s = $sql->getLine('SELECT * FROM `wb_music` WHERE `albumid`=\''.$u[1].'\'');
-if($s['albumid']){
+$s = $sql->getLine('SELECT * FROM `music_list` WHERE `albumId`=\''.$u[1].'\'');
+if($s['albumId']){
 	$l = str_replace('\\', '\\\\', json_encode($list));
-	$sql->runSql('UPDATE `wb_music` SET `list`=\''.json_encode($l).'\' WHERE `albumid`=\''.$u[1].'\'');
+	$sql->runSql('UPDATE `music_list` SET `list`=\''.json_encode($l).'\' WHERE `albumId`=\''.$u[1].'\'');
 }else{
 	$l = str_replace('\\', '\\\\', json_encode($list));
-	$sql->runSql('INSERT INTO `wb_music` (`albumid`,`title`,`artist`,`cover`,`list`) VALUES (\''.$u[1].'\',\''.$cover[3][0].'\',\''.$artist[2][0].'\',\''.$cover[1][0].'\',\''.$l.'\')');
+	$sql->runSql('INSERT INTO `music_list` (`albumId`,`title`,`artist`,`cover`,`list`) VALUES (\''.$u[1].'\',\''.$cover[3][0].'\',\''.$artist[2][0].'\',\''.$cover[1][0].'\',\''.$l.'\')');
 }
 $arr = array();
-$data = $sql->getData('SELECT `albumid`,`title`,`artist`,`cover`,`list` FROM `wb_music` ORDER BY `albumid` DESC');
+$data = $sql->getData('SELECT `albumId`,`title`,`artist`,`cover`,`list` FROM `music_list` ORDER BY `albumId` DESC');
 if($data){
 	foreach($data as $k => $v){
-		$arr[$v['albumid']] = array(
+		$arr[$v['albumId']] = array(
 			$v['title'],
 			$v['artist'],
 			$v['cover'],
@@ -57,10 +60,10 @@ if($data){
 	}
 }
 $kv->delete('music_xiami_list');
-$kv->set('music_xiami_list', json_encode($arr));
-$res = $sql->getLine('SELECT * FROM `wb_music` WHERE `albumid`=\''.$u[1].'\'');
+$kv->set('music_xiami_list', $arr);
+$res = $sql->getLine('SELECT * FROM `music_list` WHERE `albumId`=\''.$u[1].'\'');
 $r = array(
-	'albumid' => $res['albumid'],//$sql->lastId()
+	'albumId' => $res['albumId'],//$sql->lastId()
 	'list' => json_decode($res['list'], true)
 );
 
