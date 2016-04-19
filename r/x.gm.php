be547da26685e61d3fcd0814b4e1e94ddf056d85
凉宫长门 <393117228@qq.com>
2016-04-19 13:16:46 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- r/x.gm.php
+++ r/x.gm.php
@@ -1,7 +1,7 @@
 <?php
 if(!isset($_GET['a']) && !preg_match('/^gm$/', $_GET['a']))
 	err('参数错误');
-$a = $sql->getData('SELECT `albumid`,`title`,`artist`,`cover` FROM `wb_music` ORDER BY `albumid` DESC');
+$a = $sql->getData('SELECT `albumId`,`title`,`artist`,`cover` FROM `music_list` ORDER BY `albumId` DESC');
 if($a){
 	$r = $a;
 }
