cfcbb539d13bf2b2734b59c19d44cdda2329bed4
凉宫长门 <393117228@qq.com>
2016-04-21 21:17:40 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

---
+++ lab/geturl.php
@@ -0,0 +1,23 @@
+<?php
+// $url = "http://www.baidu.com/link?url=NG5rRHoP_U6OF55nvq5Ok_6P7FQFTeKtJ1S0kVK8l68gFfeeOauHg-xNhIjWHYNs";
+$url = $_POST['url'];
+if(!empty($url)){
+	$info = parse_url($url);
+
+	$fp = fsockopen($info['host'], 80, $errno, $errstr, 30);
+	fputs($fp, 'GET '.$info['path'].'?'.$info['query'].' HTTP/1.1'."\r\n");
+	fputs($fp, 'Host: '.$info['host']."\r\n");
+	fputs($fp, 'Connection: close'."\r\n\r\n");
+	$rewrite = '';
+	while(!feof($fp)){
+		$line = fgets($fp);
+		if($line != "\r\n"){
+			if(strpos($line, 'Location:') !== false){
+				$rewrite = str_replace(array("\r", "\n", 'Location: '),'', $line);
+			}
+		}else{
+			break;
+		}
+	}
+	echo $rewrite;
+}
