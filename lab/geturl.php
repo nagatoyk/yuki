0a65e1e922b4d00688d93df63f5dccfe6f4b7747
凉宫长门 <393117228@qq.com>
2016-04-22 10:52:56 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- lab/geturl.php
+++ lab/geturl.php
@@ -26,16 +26,18 @@ if($url){
 	$info = parse_url($url);
 	echo '<pre>';
 	print_r($info);
-	print_r(curl_302($url));
+	// print_r(curl_302($url));
 	echo '</pre>';
 
-	/*$fp = fsockopen($info['host'], 80, $errno, $errstr, 30);
+	$fp = fsockopen($info['host'], 80, $errno, $errstr, 30);
 	fputs($fp, 'GET '.$info['path'].'?'.$info['query'].' HTTP/1.1'."\r\n");
 	fputs($fp, 'Host: '.$info['host']."\r\n");
 	fputs($fp, 'Connection: close'."\r\n\r\n");
 	$rewrite = '';
+	echo '<p>';
 	while(!feof($fp)){
 		$line = fgets($fp);
+		echo str_replace("\r\n", '</p><p>', $line);
 		if($line != "\r\n"){
 			if(strpos($line, 'Location:') !== false){
 				$rewrite = str_replace(array("\r", "\n", 'Location: '),'', $line);
@@ -44,5 +46,6 @@ if($url){
 			break;
 		}
 	}
-	echo $rewrite;*/
+	echo '</p>';
+	echo $rewrite;
 }
