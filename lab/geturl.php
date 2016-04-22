6c1e419ad54c79c9c04434e61c4b417c4ed05fea
凉宫长门 <393117228@qq.com>
2016-04-22 10:15:45 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- lab/geturl.php
+++ lab/geturl.php
@@ -1,6 +1,7 @@
 <?php
 // $url = "http://www.baidu.com/link?url=NG5rRHoP_U6OF55nvq5Ok_6P7FQFTeKtJ1S0kVK8l68gFfeeOauHg-xNhIjWHYNs";
 $url = isset($_POST['u']) ? $_POST['u'] : isset($_GET['u']) ? $_GET['u'] : null;
+echo $url;
 if(!empty($url)){
 	$info = parse_url($url);
 
