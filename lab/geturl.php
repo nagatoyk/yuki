84b19ce8368404b8ce04757e201e98ef0aca8302
凉宫长门 <393117228@qq.com>
2016-04-22 10:38:45 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- lab/geturl.php
+++ lab/geturl.php
@@ -1,21 +1,39 @@
 <?php
 // $url = "http://www.baidu.com/link?url=NG5rRHoP_U6OF55nvq5Ok_6P7FQFTeKtJ1S0kVK8l68gFfeeOauHg-xNhIjWHYNs";
+function curl_post_302($url, $vars = null){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $url);
+	if(is_null($vars))
+		curl_setopt($ch, CURLOPT_POST, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // 302 redirect
+	if(is_null($vars))
+		curl_setopt($ch, CURLOPT_POSTFIELDS, $vars);
+	$data = curl_exec($ch);
+	$Headers =  curl_getinfo($ch);
+	curl_close($ch);
+	if($data != $Headers){
+		return  $Headers['url'];
+	}else{
+		return false;
+	}
+}
 $url = isset($_POST['u']) ? $_POST['u'] : isset($_GET['u']) ? $_GET['u'] : null;
 echo $url;
 if($url){
 	$info = parse_url($url);
 	echo '<pre>';
 	print_r($info);
+	print_r(curl_post_302($url));
 	echo '</pre>';
-	$fp = fsockopen('ssl://'.$info['host'], 443, $errno, $errstr, 30);
+
+	/*$fp = fsockopen($info['host'], 80, $errno, $errstr, 30);
 	fputs($fp, 'GET '.$info['path'].'?'.$info['query'].' HTTP/1.1'."\r\n");
 	fputs($fp, 'Host: '.$info['host']."\r\n");
 	fputs($fp, 'Connection: close'."\r\n\r\n");
 	$rewrite = '';
-	echo '<p>';
 	while(!feof($fp)){
 		$line = fgets($fp);
-		echo str_replace("\r\n", '</p><p>', $line);
 		if($line != "\r\n"){
 			if(strpos($line, 'Location:') !== false){
 				$rewrite = str_replace(array("\r", "\n", 'Location: '),'', $line);
@@ -24,6 +42,5 @@ if($url){
 			break;
 		}
 	}
-	echo '</p>';
-	echo $rewrite;
+	echo $rewrite;*/
 }
