8c3c7d041482bbbd721ee97c220a4bed5ecfb766
凉宫长门 <393117228@qq.com>
2016-04-22 11:02:33 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- lab/geturl.php
+++ lab/geturl.php
@@ -5,29 +5,8 @@
 </form>
 <?php
 // $url = "http://www.baidu.com/link?url=NG5rRHoP_U6OF55nvq5Ok_6P7FQFTeKtJ1S0kVK8l68gFfeeOauHg-xNhIjWHYNs";
-function curl_302($url){
-	$ch = curl_init(); 
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_VERBOSE, 1);
-	// curl_setopt($ch, CURLOPT_HEADER, 1);
-	// curl_setopt($ch, CURLOPT_NOBODY, 1);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-	curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$data = curl_exec($ch);
-	$Headers = curl_getinfo($ch);
-	curl_close($ch);
-	if($data != $Headers){
-		return $Headers['url'];
-	}else{
-		return false;
-	}
-}
-$url = isset($_POST['u']) ? $_POST['u'] : isset($_GET['u']) ? $_GET['u'] : null;
-echo $url;
-if($url){
+$url = $_POST['u'];
+if(!empty($url)){
 	$info = parse_url($url);
 	echo '<pre>';
 	print_r($info);
