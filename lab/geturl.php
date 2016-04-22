83bba43179a96b15b5faba9023c2e81d717281fe
凉宫长门 <393117228@qq.com>
2016-04-22 10:18:49 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- lab/geturl.php
+++ lab/geturl.php
@@ -2,9 +2,12 @@
 // $url = "http://www.baidu.com/link?url=NG5rRHoP_U6OF55nvq5Ok_6P7FQFTeKtJ1S0kVK8l68gFfeeOauHg-xNhIjWHYNs";
 $url = isset($_POST['u']) ? $_POST['u'] : isset($_GET['u']) ? $_GET['u'] : null;
 echo $url;
-if(!empty($url)){
+if($url){
 	$info = parse_url($url);
-
+	echo '<pre>';
+	print_r($info);
+	echo '</pre>';
+	exit();
 	$fp = fsockopen($info['host'], 80, $errno, $errstr, 30);
 	fputs($fp, 'GET '.$info['path'].'?'.$info['query'].' HTTP/1.1'."\r\n");
 	fputs($fp, 'Host: '.$info['host']."\r\n");
