02cea0786b0769a8ebb47541c0b5df1b37341fd0
凉宫长门 <393117228@qq.com>
2016-04-27 11:20:36 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- lab/geturl.php
+++ lab/geturl.php
@@ -10,11 +10,14 @@ require '../r/Mysql.class.php';
 // require '../r/saetv2.ex.class.php';
 $my_token = $kv->get('my_token');
 $token = $my_token['1687199364'];
+echo '<pre>';
+print_r($my_token);
+echo '</pre>';
 // $c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
 $url = $_POST['u'];
 if(!empty($url)){
 	// echo file_get_contents('https://api.weibo.com/2/short_url/shorten.json?access_token='.$wb_id.'&url_short='.$url);
-	$ch = curl_init('https://api.weibo.com/2/short_url/shorten.json?source='.$wb_id.'&access_token='.$token['access_token'].'&url_short='.$url);
+	$ch = curl_init('https://api.weibo.com/2/short_url/shorten.json?access_token='.$token['access_token'].'&url_short='.$url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
 	$data = curl_exec($ch);
