3379b4d6b71a2e86820e227f11d0b8395aa1f56f
凉宫长门 <393117228@qq.com>
2016-04-27 12:35:02 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- lab/geturl.php
+++ lab/geturl.php
@@ -16,16 +16,15 @@ print_r($_SESSION['user']);
 print_r($my_token);
 echo '</pre>';
 // $c = new SaeTClientV2($wb_id, $wb_key, $token['access_token']);
-$url = $_POST['u'];
-if(!empty($url)){
-	// echo file_get_contents('https://api.weibo.com/2/short_url/shorten.json?access_token='.$wb_id.'&url_short='.$url);
+if(!empty($_POST)){
+	echo '<pre>';
+	print_r($_POST);
+	echo '</pre>';
+	$url = $_POST['u'];
 	$ch = curl_init('https://api.weibo.com/2/short_url/shorten.json?access_token='.$_SESSION['usre']['token']['access_token'].'&url_short='.$url.'&_='.time());
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
 	$data = curl_exec($ch);
 	curl_close($ch);
 	echo $data;
-	// $json = json_decode(file_get_contents('https://api.weibo.com/2/short_url/shorten.json?access_token='.$wb_id.'&url_short='.$url), true);
-	echo '<pre>';
-	// print_r($json);
 }
