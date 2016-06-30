c91efd31ad3e383e5695e2378ed27d08bf7a0f54
凉宫长门 <393117228@qq.com>
2016-06-30 14:51:29 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- r/x.am.php
+++ r/x.am.php
@@ -13,11 +13,8 @@ function getLen($id){
 }
 
 preg_match('/http\:\/\/www\.xiami\.com\/album\/([0-9]{5,12})/s', $_POST['url'], $u);
-$url = 'http://www.xiami.com/album/'.$u[1];
+$url = 'http://www.xiami.com/album/'.$u[1].'?_='.time();
 $file = file_get_contents($url);
-// $file = file_get($url);
-
-echo $file;
 preg_match_all('/<a[^>]*href="\/song\/([0-9]{5,12})"[^>]*title/is', $file, $data);
 preg_match('/<meta[^>]*property="og\:title" content="(.*?)"\/>/is', $file, $title);
 preg_match('/<meta[^>]*property="og\:music\:artist" content="(.*?)"\/>/', $file, $artist);
@@ -32,21 +29,10 @@ for($i = 0; $i < count($data[1]); $i++){
 		);
 	}
 }
-print_r($data);
-// echo json_encode($list);
-die();
 $s = $sql->getLine('SELECT * FROM `music_list` WHERE `albumId`=\''.$u[1].'\'');
 if($s['albumId']){
-	// $l = str_replace('\\', '\\\\', json_encode($list));
 	$sql->runSql('UPDATE `music_list` SET `list`=\''.serialize($list).'\',`title`=\''.$title[1].'\',`artist`=\''.$artist[1].'\',`cover`=\''.$cover[1][0].'\' WHERE `albumId`=\''.$u[1].'\'');
 }else{
-	/*array(
-						'albumId'=>$u[1],
-						'title'=>$title[1],
-						'artist'=>$artist[1],
-						'cover'=>$cover[1][0],
-						'list'=>serialize($list))*/
-	// $l = str_replace('\\', '\\\\', json_encode($list));
 	$sql->runSql('INSERT INTO `music_list` (`albumId`,`title`,`artist`,`cover`,`list`) VALUES (\''.$u[1].'\',\''.$title[1].'\',\''.$artist[1].'\',\''.$cover[1][0].'\',\''.serialize($list).'\')');
 }
 $arr = array();
@@ -63,8 +49,9 @@ if($data){
 }
 $kv->delete('music_xiami_list');
 $kv->set('music_xiami_list', $arr);
+$res = $sql->getLine('SELECT `list` FROM `music_list` WHERE `albumId`=\''.$u[1].'\'');
 $r = array(
 	'albumId' => $u[1],//$sql->lastId()
-	'list' => $list
+	'list' => unserialize($res['list'])
 );
 
