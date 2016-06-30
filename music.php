3389580ff572afda8177c7ae0088eb4048c70330
凉宫长门 <393117228@qq.com>
2016-06-30 15:00:48 +0800

    修改 music.php
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- music.php
+++ music.php
@@ -1,20 +1,19 @@
 <?php
 require 'r/fun.php';
 require 'x/mysql.class.php';
+$kv->delete('music_xiami_list');
 $r = $kv->get('music_xiami_list');
 if(!$r){
 	$arr = array();
 	$data = $sql->getData('SELECT `albumId`,`title`,`artist`,`cover`,`list` FROM `music_list` ORDER BY `albumId` DESC');
 	if($data){
 		foreach($data as $k => $v){
-			$list = unserialize($v['list']);
-			// $list = json_decode($v['list'], true);
 			if(count($list) > 0){
 				$arr[$v['albumId']] = array(
 					$v['title'],
 					$v['artist'],
 					$v['cover'],
-					$list
+					unserialize($v['list'])
 				);
 			}
 		}
@@ -23,5 +22,4 @@ if(!$r){
 	$r = $arr;
 }
 header('Content-type: application/json');
-// echo str_replace('&amp;', '&', str_replace('&#039;', '\'', json_encode($r)));
 echo json_encode($r);
