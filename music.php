a644c617fe5e7745f0a1fccc80a39ce90a0182c2
凉宫长门 <393117228@qq.com>
2016-06-30 15:02:01 +0800

    修改 music.php
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- music.php
+++ music.php
@@ -1,7 +1,8 @@
 <?php
 require 'r/fun.php';
 require 'x/mysql.class.php';
-$kv->delete('music_xiami_list');
+if(isset($_GET['m']) && $_GET['m'] = 'del')
+	$kv->delete('music_xiami_list');
 $r = $kv->get('music_xiami_list');
 if(!$r){
 	$arr = array();
