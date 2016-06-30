a09b4a6940a9069e0115c71360a90f1a7fddadd0
凉宫长门 <393117228@qq.com>
2016-06-30 08:36:17 +0800

    修改

--- r/x.dm.php
+++ r/x.dm.php
@@ -1,5 +1,8 @@
 <?php
-if(isset($_POST['albumId']) && is_numeric($_POST['albumId']) && $_POST['albumId'] > 0){
-	$sql->runSql('DELETE FROM music_list WHERE albumId='.$_POST['albumId']);
-	$r = array('msg'=>'删除成功');
-}
+if(!isset($_GET['a']) && !preg_match('/^dm$/', $_GET['a']))
+	err('参数错误');
+if(!preg_match('/^[0-9]+$/', $_POST['albumId']))
+	err('您输入的 [albumId] 格式不正确！');
+$sql->runSql('DELETE FROM `music_list` WHERE `albumId`=\''.$_POST['albumId'].'\'');
+$r = array('msg'=>'删除成功');
+
