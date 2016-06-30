2d25bc0346a3a07688f62f5bef5c2d6d4e5250a1
凉宫长门 <393117228@qq.com>
2016-06-30 08:29:16 +0800

    修改

--- r/x.dm.php
+++ r/x.dm.php
@@ -1,5 +1,5 @@
 <?php
 if(isset($_POST['albumId']) && is_numeric($_POST['albumId']) && $_POST['albumId'] > 0){
 	$sql->runSql('DELETE FROM music_list WHERE albumId='.$_POST['albumId']);
-	$r = array('msg'=>$sql->affectedRows());
+	$r = array('msg'=>'删除成功');
 }
