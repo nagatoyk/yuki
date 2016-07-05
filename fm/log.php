c722851e977d0c248cc372e475b91ce40acf5730
凉宫长门 <393117228@qq.com>
2016-07-05 09:17:25 +0800

    修改 fm/log.php
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- fm/log.php
+++ fm/log.php
@@ -7,9 +7,18 @@ if(preg_match('/^[0-9]{5,12}$/', $pid) && preg_match('/^[0-9]{1,3}$/', $rid)){
 	$data = $sql->getLine('SELECT * FROM imouto_playcount WHERE pid=\''.$pid.'\' AND rid=\''.$rid.'\'');
 	header('Content-type: application/json;charset=utf-8');
 	if($data){
-		// 
-		echo json_encode($data);
+		$sql->runSql('UPDATE imouto_playcount SET pcount=pcount+1 WHERE pid=\''.$pid.'\' AND rid=\''.$rid.'\'');
+		echo json_encode(array(
+			'id'=>$pid,
+			'msg'=>'更新记录成功!',
+			'count'=>($data['pcount'] + 1)
+		));
 	}else{
-		echo json_encode(array('error'=>'无记录'));
+		$sql->runSql('INSERT INTO imouto_playcount (`pid`,`rid`,`pcount`) VALUES ('.$pid.','.$rid.',1)');
+		echo json_encode(array(
+			'id'=>$sql->lastId(),
+			'msg'=>'添加记录成功!',
+			'count'=>1
+		));
 	}
 }
