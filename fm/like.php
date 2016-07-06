7e1f851197948c730b98ad1416773f13daa5b1d3
凉宫长门 <393117228@qq.com>
2016-07-06 22:02:18 +0800

    修改 fm/like.php
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- fm/like.php
+++ fm/like.php
@@ -6,12 +6,9 @@ $key = '18f95c02504fb5a0fdd83b205e7e1aee05421a58b';
 $secret = 'a3af2e9f06faaefb9408897388f0f916';
 $callback = 'http://kloli.tk/fm/login.php';
 $MoeFM = new MoeFM($key, $secret, $callback);
-echo '<pre>';
-print_r($_SESSION);
-echo '</pre>';
 if($_GET['a'] == 'like'){
 	$pid = $_POST['pid'];
 	$rid = $_POST['rid'];
-	$result = $MoeFM->set_fav($_SESSION['moefou']['oauth_token'], $_SESSION['moefou']['oauth_token_secret'], 1, $pid);
+	$result = $MoeFM->add_like_fav($_SESSION['moefou']['oauth_token'], $_SESSION['moefou']['oauth_token_secret'], $pid);
 	print_r($result);
 }
