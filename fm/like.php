0b7f10b11caf189bd676fe75f6568f158df2b503
凉宫长门 <393117228@qq.com>
2016-07-06 21:55:21 +0800

    修改 fm/like.php
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- fm/like.php
+++ fm/like.php
@@ -2,11 +2,13 @@
 session_start();
 require '../r/fun.php';
 require '../lab/moefou.class.php';
-require '../x/mysql.class.php';
 $key = '18f95c02504fb5a0fdd83b205e7e1aee05421a58b';
 $secret = 'a3af2e9f06faaefb9408897388f0f916';
 $callback = 'http://kloli.tk/fm/login.php';
 $MoeFM = new MoeFM($key, $secret, $callback);
+echo '<pre>';
+print_r($_SESSION);
+echo '</pre>'
 if($_GET['a'] == 'like'){
 	$pid = $_POST['pid'];
 	$rid = $_POST['rid'];
