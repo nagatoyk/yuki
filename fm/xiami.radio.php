0aefb090df6811e8371a8d3863d5b23af2c8c406
凉宫长门 <393117228@qq.com>
2016-07-06 14:47:16 +0800

    修改 fm/xiami.radio.php
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- fm/xiami.radio.php
+++ fm/xiami.radio.php
@@ -23,7 +23,7 @@ function get_xml($url){
 	if(!curl_exec($ch)){
 		$errno = curl_errno($ch);
 		writelog('抓取失败, 错误码->'.$errno);
-		$data = '';
+		$data = false;
 	}else{
 		$data = curl_multi_getcontent($ch);
 	}
@@ -34,47 +34,43 @@ function get_playcount($sql, $pid, $rid = 11){
 	$r = $sql->getLine('SELECT `pcount` FROM imouto_playcount WHERE pid=\''.$pid.'\' AND rid=\''.$rid.'\'');
 	return $r ? $r['pcount'] : 0;
 }
-function xmltoarr($xml){
-	$out = array();
-	$doc = new DOMDocument();
-	$doc->loadXML($xml);
-	$items = $doc->getElementsByTagName('track');
-	foreach($items as $node){
-		$song_ids = $node->getElementsByTagName('song_id');
-		$titles = $node->getElementsByTagName('title');
-		$pics = $node->getElementsByTagName('pic');
-		$locations = $node->getElementsByTagName('location');
-		$album_names = $node->getElementsByTagName('album_name');
-		$artists = $node->getElementsByTagName('artist');
-		$album_ids = $node->getElementsByTagName('album_id');
-		$lengths = $node->getElementsByTagName('length');
-		$song_id = $song_ids->item(0)->nodeValue;
-		$title = $titles->item(0)->nodeValue;
-		$pic = $pics->item(0)->nodeValue;
-		$location = $locations->item(0)->nodeValue;
-		$artist = $artists->item(0)->nodeValue;
-		$album_name = $album_names->item(0)->nodeValue;
-		$album_id = $album_ids->item(0)->nodeValue;
-		$length = $lengths->item(0)->nodeValue;
-		$out[] = array(
-			'xid'=>$song_id,
-			'title'=>htmlspecialchars_decode($title, ENT_QUOTES),
-			'img'=>str_replace('http://img.xiami.net/images/album/', '', $pic),
-			'mp3'=>$location,
-			'album_name'=>htmlspecialchars_decode($album_name, ENT_QUOTES),
-			'artist'=>htmlspecialchars_decode($artist, ENT_QUOTES),
-			'album_id'=>$album_id,
-			'length'=>$length,
-			'play'=>get_playcount($sql, $song_id)
-		);
-	}
-	return $out;
-}
+$out = array();
 if($_GET['a'] == 'radio' && $_GET['rid'] == 11){
-	$url = 'http://www.xiami.com/radio/xml/type/8/id/6961722';
+	$url = 'http://www.xiami.com/radio/xml/type/8/id/6961722?v='.time();
 	$xml = get_xml($url);
-	if($xml != ''){
-		$out = xmltoarr($xml);
+	if($xml){
+		$doc = new DOMDocument();
+		$doc->loadXML($xml);
+		$items = $doc->getElementsByTagName('track');
+		foreach($items as $node){
+			$song_ids = $node->getElementsByTagName('song_id');
+			$titles = $node->getElementsByTagName('title');
+			$pics = $node->getElementsByTagName('pic');
+			$locations = $node->getElementsByTagName('location');
+			$album_names = $node->getElementsByTagName('album_name');
+			$artists = $node->getElementsByTagName('artist');
+			$album_ids = $node->getElementsByTagName('album_id');
+			$lengths = $node->getElementsByTagName('length');
+			$song_id = $song_ids->item(0)->nodeValue;
+			$title = $titles->item(0)->nodeValue;
+			$pic = $pics->item(0)->nodeValue;
+			$location = $locations->item(0)->nodeValue;
+			$artist = $artists->item(0)->nodeValue;
+			$album_name = $album_names->item(0)->nodeValue;
+			$album_id = $album_ids->item(0)->nodeValue;
+			$length = $lengths->item(0)->nodeValue;
+			$out[] = array(
+				'xid'=>$song_id,
+				'title'=>htmlspecialchars_decode($title, ENT_QUOTES),
+				'img'=>str_replace('http://img.xiami.net/images/album/', '', $pic),
+				'mp3'=>$location,
+				'album_name'=>htmlspecialchars_decode($album_name, ENT_QUOTES),
+				'artist'=>htmlspecialchars_decode($artist, ENT_QUOTES),
+				'album_id'=>$album_id,
+				'length'=>$length,
+				'play'=>get_playcount($sql, $song_id)
+			);
+		}
 	}else{
 		$out = array();
 	}
