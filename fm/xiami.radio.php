48ef3a1cb98eb5694e4cbbca2303ba072ff4ba7d
凉宫长门 <393117228@qq.com>
2016-07-06 16:02:49 +0800

    修改 fm/xiami.radio.php
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- fm/xiami.radio.php
+++ fm/xiami.radio.php
@@ -25,7 +25,7 @@ function get_xml($url){
 		'Cookie: gid=146779186835297; _xiamitoken=36fba1f459be524f64a15b4f0fe9a4ab; _unsign_token=8fcb821a564a40d2658fd9dddaa984a6; cna=VtugD0Sg10cCATs9NEDzOAwl; member_auth=1GnLE9tCv25i1%2FeTSIA4InVL6LLQEmOCwI5Y3uN8sFN3J4laMourlquXSw1J3yKRkYG6uGUmXO3Omzjxk%2BCh; user=6961722%22%E9%95%9C%E8%8A%B1%E6%B0%B4%E6%9C%88%22images%2Favatar_new%2F139%2F23%2F6961722%2F6961722_1322238218_1.jpg%220%227376%22%3Ca+href%3D%27%2Fwebsitehelp%23help9_3%27+%3Esi%3C%2Fa%3E%2220%22357%2223798%2246ba2b68d2%221467791879; CNZZDATA921634=cnzz_eid%3D472386582-1467787454-http%253A%252F%252Fwww.xiami.com%252F%26ntime%3D1467787454; CNZZDATA2629111=cnzz_eid%3D735352521-1467787455-http%253A%252F%252Fwww.xiami.com%252F%26ntime%3D1467787455; t_sign_auth=1; l=AouL3BUbfWB3nhraTKSAhqtCmyV1IJ-i; isg=AsjIp2FOC5cEymcbrIaaCHQsmTZVzSx7xwmh44J5FMM2XWjHKoH8C15dq4LX',
 		'Host:www.xiami.com',
 		// 'Referer:http://www.xiami.com/radio/play/type/4/oid/6961722',
-		'User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.19 Safari/537.36'
+		// 'User-Agent:Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.19 Safari/537.36'
 	));
 	if(!curl_exec($ch)){
 		$errno = curl_errno($ch);
