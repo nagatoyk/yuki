cc93d6e2037414010365deead57a7cc1eb9219a4
凉宫长门 <393117228@qq.com>
2016-07-06 13:38:49 +0800

    修改 xiami.radio.php
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- fm/xiami.radio.php
+++ fm/xiami.radio.php
@@ -39,7 +39,7 @@ function writelog($str){
 function get_xml($url){
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_HEADER, true)
+	curl_setopt($ch, CURLOPT_HEADER, true);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
