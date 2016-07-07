b6de9525605f1eacbfb4e6b5974e1000aaf54cf0
凉宫长门 <393117228@qq.com>
2016-07-07 20:54:01 +0800

    添加

---
+++ app/home/controller/Index.php
@@ -0,0 +1,26 @@
+<?php
+/** .-------------------------------------------------------------------
+ * |  Software: [HDCMS framework]
+ * |      Site: www.hdcms.com
+ * |-------------------------------------------------------------------
+ * |    Author: 向军 <2300071698@qq.com>
+ * |    WeChat: aihoudun
+ * | Copyright (c) 2012-2019, www.houdunwang.com. All Rights Reserved.
+ * '-------------------------------------------------------------------*/
+namespace app\home\controller;
+class Index {
+	public function index() {
+		echo u('fm.index.index');
+		echo '<hr>';
+		$MoeFM = new \app\org\MoefouOAuth('18f95c02504fb5a0fdd83b205e7e1aee05421a58b', 'a3af2e9f06faaefb9408897388f0f916', u('login'));
+		$url = $MoeFM->getAuthorizeURL();
+		echo $url;
+		\View::make();
+	}
+	public function login() {
+		p(q('get.'));
+	}
+	public function show() {
+		\View::make();
+	}
+}
