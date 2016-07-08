73ef4c2251accda27ecba3fa82915904ddb04a95
凉宫长门 <393117228@qq.com>
2016-07-08 13:02:46 +0800

    修改
    Signed-off-by: 凉宫长门 <393117228@qq.com>

--- lab/mysql.php
+++ lab/mysql.php
@@ -8,16 +8,16 @@
 	echo $host.'/'.$port.'/'.$dbname.'/'.$user.'/'.$pwd;
 	$url = 'http://www.xiami.com/radio/xml/type/4/id/6961722?_='.time();
 	echo file_get_contents($url);
-$file = fopen('../data/fm_getxml_log.txt', 'r');
-$user = array();
-$i = 0;
-//输出文本中所有的行，直到文件结束为止。
-while(!feof($file)){
-	$user[$i] = fgets($file);//fgets()函数从文件指针中读取一行
-	$i++;
-}
-fclose($file);
-$user = array_filter($user);
-echo '<pre>';
-print_r($user);
-echo '</pre>';
+	$file = fopen('../data/fm_getxml_log.txt', 'r');
+	$user = array();
+	$i = 0;
+	//输出文本中所有的行，直到文件结束为止。
+	while(!feof($file)){
+		$user[$i] = fgets($file);//fgets()函数从文件指针中读取一行
+		$i++;
+	}
+	fclose($file);
+	$user = array_filter($user);
+	echo '<pre>';
+	print_r($user);
+	echo '</pre>';
