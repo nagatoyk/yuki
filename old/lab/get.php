700e10066cee1b284522f194a900807b7f70d45e
凉宫长门 <393117228@qq.com>
2025-03-01 18:07:37 +0800

    generate pelican site

--- old/lab/get.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(isset($_GET)){
-	$url = 'http://zh.moegirl.org/index.php?'.$_SERVER['QUERY_STRING'];
-	$file = file_get_contents($url);
-	echo $file;
-}
-
--- r/x.dp.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if(!isset($_POST['pid'])||!preg_match('/^[0-9]{1,15}$/',$_POST['pid']))
-	err('您要删除的文章不存在');
-
-
-$sql->runSql('DELETE FROM imouto_article WHERE pid='.$_POST['pid']);
-
-$r['pid']=$_POST['pid'];
