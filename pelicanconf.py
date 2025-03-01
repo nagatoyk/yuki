d1fe9d8f0dea4dc1684422d03d1e446d445060c8
凉宫长门 <393117228@qq.com>
2025-03-01 20:45:36 +0800

    cs

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,6 +1,6 @@
 AUTHOR = 'Suzumiya Nagato'
 SITENAME = "Suzumiya Nagato's Blog"
-SITEURL = "."
+SITEURL = ""
 
 PATH = "content"
 
@@ -34,4 +34,19 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
 
-OUTPUT_PATH = "docs"
+OUTPUT_PATH = 'docs'
+
+THEME = 'themes/pelican-alchemy/alchemy'
+SITEIMAGE = 'https://gravatar.loli.net/avatar/6cf979ad5605b762e43104e1b2c27ad3?s=320'
+PYGMENTS_STYLE = 'friendly'
+# BOOTSTRAP_CSS = 'https://bootswatch.com/5/lux/bootstrap.min.css'
+DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'sitemap']
+SITEMAP_SAVE_AS = 'sitemap.xml'
+ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = ARTICLE_URL
+DRAFTS_URL = 'drafts/{date:%Y}/{date:%m}/{slug}.html'
+DRAFTS_SAVE_AS = ARTICLE_URL
+PAGE_URL = 'pages/{slug}.html'
+PAGE_SAVE_AS = PAGE_URL
+
+FEED_ALL_RSS = 'feeds/all.rss.xml'
