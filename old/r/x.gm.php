55d8503db7642f2adf4d181731286c0c8f2f9bce
凉宫长门 <393117228@qq.com>
2025-03-01 17:52:18 +0800

    generate pelican site

--- old/r/x.gm.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(!isset($_GET['a']) && !preg_match('/^gm$/', $_GET['a']))
-	err('参数错误');
-$a = $sql->getData('SELECT `albumId`,`title`,`artist`,`cover` FROM `music_list` ORDER BY `albumId` DESC');
-if($a){
-	$r = $a;
-}
--- x/x.stat.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-header('Content-type: application/json;charset=utf-8');
-
-print_r($sql->getData('SELECT * FROM imouto_stats LIMIT 0,100'));
-
-exit();
